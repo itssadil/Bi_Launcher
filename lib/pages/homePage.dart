6cf8138b9745ebdcfa78e6343890b7681f86eb7b
itssadil <adilhussain.dev@gmail.com>
2023-04-19 16:25:23 +0600

    Refresh

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -4,26 +4,41 @@ import 'subPage/appsArea.dart';
 import 'subPage/headerArea.dart';
 import 'subPage/sidebar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Future<void> _handleRefresh() async {
+    // Add the logic to reload the data or refresh the UI here
+    setState(() {
+      // Update your app's data here
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Stack(
-          children: [
-            Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                HeaderArea(),
-                AppsArea(),
-              ],
+      child: Stack(
+        children: [
+          RefreshIndicator(
+            onRefresh: _handleRefresh,
+            child: SingleChildScrollView(
+              physics: BouncingScrollPhysics(),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  HeaderArea(),
+                  AppsArea(),
+                ],
+              ),
             ),
-            Sidebar(),
-          ],
-        ),
+          ),
+          Sidebar(),
+        ],
       ),
     );
   }
