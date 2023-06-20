c067c0174ed3b69dcc7012c273a35724ea064401
itssadil <adilhussain.dev@gmail.com>
2023-06-20 16:31:46 +0600

    Native Splash added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-// import 'package:bi_launcher/pages/homePage.dart';
+
 import 'package:bi_launcher/pages/homePage.dart';
 import 'package:bi_launcher/pages/searchPage.dart';
 import 'package:bi_launcher/providers/favProvider.dart';
 import 'package:bi_launcher/providers/sectionControls.dart';
-// import 'package:bi_launcher/widgets/allApps.dart';
 import 'package:bi_launcher/widgets/myDrawer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
